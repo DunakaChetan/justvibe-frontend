db38df3f88cce60db94d76e975e6859b13fe4eb1
Dunaka Chetan <150909694+DunakaChetan@users.noreply.github.com>
2025-09-24 10:52:10 +0530

    Update Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -83,7 +83,7 @@ function Header() {
     return (
         <div id='header'>
             <div id='logo' onClick={homepage}>
-                <span>Music</span>Vibe
+                <span>Music</span>Site
             </div>
             <div id='userinfo'>
                 <div className="animated-search-icon" onClick={handleSearchIconClick}>
